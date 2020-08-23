2c287d6c20241ce379364e3307591c45393771f8
Ajak Cyer <ajakcyer97@gmail.com>
2020-08-23 15:19:50 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,9 +12,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def move(board, index, character = "X")
   board[index] = character
 end
